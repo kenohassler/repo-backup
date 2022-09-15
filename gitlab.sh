d36c9efa9dfe28e0c16119f8c234d4cb31a09a50
Keno Hassler <40292329+kenohassler@users.noreply.github.com>
2022-09-15 22:24:48 +0200

    backup groups

--- gitlab.sh
+++ gitlab.sh
@@ -7,11 +7,32 @@ cd $dirname
 #change these vars:
 privateToken=?
 userName=?
+url=gitlab.com
 
-curl --header "Private-Token: $privateToken" "https://gitlab.com/api/v4/users/$userName/projects" \
-   | jq -r '.[] | .id, .name' \
-   | while IFS= read projectId; read projectName; do
-        curl --header "Private-Token: $privateToken" "https://gitlab.com/api/v4/projects/$projectId/repository/archive.zip" --output $projectName.zip
-    done
+curl --header "Private-Token: $privateToken" "https://$url/api/v4/users/$userName/projects" | #  \
+	jq -r '.[] | .id, .name' |
+	while
+		IFS= read projectId
+		read projectName
+	do
+		curl --header "Private-Token: $privateToken" "https://$url/api/v4/projects/$projectId/repository/archive.zip" --output $projectName.zip
+	done
+
+curl --header "Private-Token: $privateToken" "https://$url/api/v4/groups" |
+	jq -r '.[] | .id, .name' |
+	while
+		IFS= read groupId
+		read groupName
+	do
+		curl --header "Private-Token: $privateToken" "https://$url/api/v4/groups/$groupId/projects" | #  \
+			jq -r '.[] | .id, .name' |
+			while
+				IFS= read projectId
+				read projectName
+			do
+				mkdir -p $groupName
+				curl --header "Private-Token: $privateToken" "https://$url/api/v4/projects/$projectId/repository/archive.zip" --output $groupName/$projectName.zip
+			done
+	done
 
 echo Done! All files downloaded here: $(pwd)
